147580173969cb06911b4b5bb8523f80fde20ce0
Rodolfo Boer <rodolfo.boer@gmail.com>
2018-12-26 22:19:41 +0100

    init

---
+++ src/bin/template.rs
@@ -0,0 +1,32 @@
+use std::fs;
+use std::io;
+
+fn first(input: &Vec<&str>) -> u32 {
+    unimplemented!()
+}
+
+fn second(input: &Vec<&str>) -> u32 {
+    unimplemented!()
+}
+
+fn main() {
+    let input = read_file("01");
+    let input: Vec<&str> = input.trim().split("\n").collect();
+
+    println!("{}", first(&input));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn aaa() {
+        assert_eq!(first(&vec!["a", "a", "a"]), 3);
+    }
+}
+
+fn read_file(day: &str) -> String {
+    let path = format!("input/{}.txt", day);
+    fs::read_to_string(path).unwrap()
+}
