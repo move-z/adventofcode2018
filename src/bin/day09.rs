917a4453bf759d68a9ea829760613e03c64dad4d
Rodolfo Boer <rodolfo.boer@gmail.com>
2019-02-27 23:12:10 +0100

    day 9 part 2

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -2,34 +2,31 @@ extern crate adventofcode2018;
 #[macro_use]
 extern crate lazy_static;
 
+use std::cell::RefCell;
+use std::rc::Rc;
+
 use regex::Regex;
 
 use adventofcode2018::*;
 
 fn first(player_num: usize, max_value: usize) -> usize {
-    let mut players = Vec::with_capacity(4);
+    let mut players = Vec::with_capacity(player_num);
     for _ in 0..player_num {
         players.push(0);
     }
 
-    let mut board = vec![0];
+    let mut board = List::new(0);
 
-    let mut cur = 0;
-    let mut cur_idx = 0;
-
-    while cur < max_value {
-        cur += 1;
+    for cur in 1..=max_value {
         if cur % 23 == 0 {
-            let take_idx = (board.len() + cur_idx - 7) % board.len();
-            let take = board.remove(take_idx);
+            board.reverse(7);
+            let take = board.remove();
 
             let val = players.get_mut(cur % player_num).unwrap();
             *val = *val + take + cur;
-
-            cur_idx = take_idx;
         } else {
-            cur_idx = (cur_idx + 2) % board.len();
-            board.insert(cur_idx, cur);
+            board.advance(2);
+            board.insert(cur);
         }
     }
 
@@ -37,7 +34,72 @@ fn first(player_num: usize, max_value: usize) -> usize {
 }
 
 fn second(player_num: usize, max_value: usize) -> usize {
-    unimplemented!()
+    first(player_num, max_value * 100)
+}
+
+struct Entry {
+    value: usize,
+    next: Option<Rc<RefCell<Entry>>>,
+    prev: Option<Rc<RefCell<Entry>>>
+}
+
+struct List {
+    head: Rc<RefCell<Entry>>
+}
+
+impl List {
+    fn new(value: usize) -> List {
+        let head = Entry { value, next: None, prev: None };
+        let rc = Rc::new(RefCell::new(head));
+        rc.borrow_mut().next = Some(rc.clone());
+        rc.borrow_mut().prev = Some(rc.clone());
+        List { head: rc }
+    }
+
+    fn insert(&mut self, value: usize) {
+        let mut head = Entry { value, next: None, prev: None };
+        let next = self.head.clone();
+        let prev = next.borrow().prev.clone().unwrap();
+        head.next = Some(next);
+        head.prev = Some(prev);
+
+        let head = Rc::new(RefCell::new(head));
+        let next = self.head.clone();
+        let prev = next.borrow().prev.clone().unwrap();
+        next.borrow_mut().prev = Some(head.clone());
+        prev.borrow_mut().next = Some(head.clone());
+
+        self.head = head;
+    }
+
+    fn remove(&mut self) -> usize {
+        let head = self.head.clone();
+
+        let next = head.borrow().next.clone().unwrap();
+        let prev = head.borrow().prev.clone().unwrap();
+        next.borrow_mut().prev = Some(prev.clone());
+        prev.borrow_mut().next = Some(next.clone());
+        self.head = next.clone();
+
+        let v = head.borrow().value;
+        v
+    }
+
+    fn advance(&mut self, amount: usize) {
+        let head = self.head.clone();
+        self.head = head.borrow().next.clone().unwrap();
+        if amount > 1 {
+            self.advance(amount - 1);
+        }
+    }
+
+    fn reverse(&mut self, amount: usize) {
+        let head = self.head.clone();
+        self.head = head.borrow().prev.clone().unwrap();
+        if amount > 1 {
+            self.reverse(amount - 1);
+        }
+    }
 }
 
 lazy_static! {
@@ -54,6 +116,8 @@ fn main() {
         let max_value = parse_capture::<usize>(&cap, 2, "max_value").unwrap();
 
         println!("{}", first(players, max_value));
+
+        println!("{}", second(players, max_value));
     }
 
     println!("elapsed {:?}", start.elapsed());
