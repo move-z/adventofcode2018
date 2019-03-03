e155b55c81757d20c1465a1130055beb785e43c4
Rodolfo Boer <rodolfo.boer@gmail.com>
2019-03-03 13:42:54 +0100

    day 12 part 2

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -15,7 +15,7 @@ fn first(initial: &str, rules: &HashMap<&str, char>) -> isize {
             current.push_str("....");
         }
 
-        current = next_state(current, rules);
+        current = next_state(&current, rules);
     }
 
     current.char_indices()
@@ -26,11 +26,52 @@ fn first(initial: &str, rules: &HashMap<&str, char>) -> isize {
         .sum()
 }
 
-fn second(_input: &Vec<&str>) -> u32 {
-    unimplemented!()
+fn second(initial: &str, rules: &HashMap<&str, char>) -> i64 {
+    let mut current = String::from(initial);
+    let mut start_idx = 0;
+
+    fn count(current: &String, start_idx: isize) -> i64 {
+        current.char_indices()
+            .filter(|(_, c)| *c == '#')
+            .map(|(i, _)| {
+                i as i64 + start_idx as i64
+            })
+            .sum()
+    }
+
+    let mut visited = HashMap::new();
+
+    const LIMIT: u64 = 50_000_000_000;
+    let mut i = 0u64;
+    while i <= LIMIT {
+        visited.insert(current.trim_matches('.').to_owned(), (i, count(&current, start_idx)));
+
+        if !current.starts_with("....") {
+            current.insert_str(0, "....");
+            start_idx -= 4;
+        }
+        if !current.ends_with("....") {
+            current.push_str("....");
+        }
+
+        current = next_state(&current, rules);
+        i += 1;
+
+        if let Some((prev_idx, prev_val)) = visited.get(current.trim_matches('.')) {
+            let cur_val = count(&current, start_idx);
+            let loop_size = i - prev_idx;
+            let val_diff = cur_val - prev_val;
+            let loops = (LIMIT - i) / loop_size;
+            if i + loop_size * loops == LIMIT {
+                return cur_val + val_diff * loops as i64;
+            }
+        }
+    }
+
+    count(&current, start_idx)
 }
 
-fn next_state(cur: String, rules: &HashMap<&str, char>) -> String {
+fn next_state(cur: &String, rules: &HashMap<&str, char>) -> String {
     let mut new = String::from(&cur[..2]);
     for i in 0..cur.len() - 5 {
         let slice = &cur[i..i + 5];
@@ -63,6 +104,8 @@ fn main() {
     });
 
     println!("{}", first(initial, &rules));
+
+    println!("{}", second(initial, &rules));
 
     println!("elapsed {:?}", start.elapsed());
 }
