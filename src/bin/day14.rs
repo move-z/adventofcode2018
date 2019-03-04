f2e6524f44766dcdc7ad3a8c987e06cac99178c8
Rodolfo Boer <rodolfo.boer@gmail.com>
2019-03-04 13:17:33 +0100

    day 14 part 2

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -9,7 +9,28 @@ fn first(input: usize) -> String {
         indices = next(&mut recipes, indices);
     }
 
-    recipes[input..input+10].iter().join("")
+    recipes[input..input + 10].iter().join("")
+}
+
+fn second(input: &str) -> usize {
+    let target = input
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as u8)
+        .collect::<Vec<u8>>();
+    let mut recipes = vec![3, 7];
+    let mut indices = (0, 1);
+
+    loop {
+        indices = next(&mut recipes, indices);
+        if recipes.len() > target.len() {
+            let startidx = recipes.len() - target.len() - 1;
+            if recipes[startidx + 1..] == target[..] {
+                return recipes.len() - target.len();
+            } else if recipes[startidx..recipes.len() - 1] == target[..] {
+                return recipes.len() - target.len() - 1;
+            }
+        }
+    }
 }
 
 fn next(recipes: &mut Vec<u8>, indices: (usize, usize)) -> (usize, usize) {
@@ -20,9 +41,7 @@ fn next(recipes: &mut Vec<u8>, indices: (usize, usize)) -> (usize, usize) {
     }
     recipes.push(n);
 
-    let next_idx = |idx| {
-        (idx + 1 + usize::from(recipes[idx])) % recipes.len()
-    };
+    let next_idx = |idx| (idx + 1 + usize::from(recipes[idx])) % recipes.len();
 
     (next_idx(indices.0), next_idx(indices.1))
 }
@@ -30,9 +49,12 @@ fn next(recipes: &mut Vec<u8>, indices: (usize, usize)) -> (usize, usize) {
 fn main() {
     let start = std::time::Instant::now();
 
-    let input = read_file("14").trim().parse::<usize>().unwrap();
+    let input = read_file("14");
+    let input = input.trim();
 
-    println!("{}", first(input));
+    println!("{}", first(input.parse::<usize>().unwrap()));
+
+    println!("{}", second(input));
 
     println!("elapsed {:?}", start.elapsed());
 }
@@ -59,5 +81,23 @@ mod test {
     #[test]
     fn test2018() {
         assert_eq!(first(2018), "5941429882");
+    }
+
+    #[test]
+    fn test51589() {
+        assert_eq!(second("51589"), 9);
+    }
+
+    #[test]
+    fn test01245() {
+        assert_eq!(second("01245"), 5);
+    }
+    #[test]
+    fn test92510() {
+        assert_eq!(second("92510"), 18);
+    }
+    #[test]
+    fn test59414() {
+        assert_eq!(second("59414"), 2018);
     }
 }
