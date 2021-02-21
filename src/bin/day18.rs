89b97e77d328a46f69b87c56634195b0e2cf3960
Rodolfo Boer <rodolfo.boer@gmail.com>
2021-02-21 23:10:06 +0100

    day 18 part 1
    Signed-off-by: Rodolfo Boer <rodolfo.boer@gmail.com>

---
+++ src/bin/day18.rs
@@ -0,0 +1,230 @@
+use adventofcode2018::*;
+
+fn first(input: &[&str]) -> usize {
+    let mut area = CollectionArea::parse(input).unwrap();
+    for _ in 0..10 {
+        area = area.next();
+    }
+    area.resource_value()
+}
+
+fn second(_input: &[&str]) -> usize {
+    unimplemented!()
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+enum Acre {
+    OpenGround,
+    Trees,
+    Lumberyard,
+}
+
+impl Acre {
+    fn parse(input: &char) -> Result<Acre, String> {
+        match input {
+            '.' => Ok(Acre::OpenGround),
+            '|' => Ok(Acre::Trees),
+            '#' => Ok(Acre::Lumberyard),
+            c => Err(format!("can't parse {} as Acre", c)),
+        }
+    }
+
+    fn next_state(&self, adjacent: &[&Acre]) -> Acre {
+        let count = |t| adjacent.iter().filter(|&&&a| a == t).count();
+        match self {
+            Acre::OpenGround => {
+                if count(Acre::Trees) >= 3 {
+                    Acre::Trees
+                } else {
+                    Acre::OpenGround
+                }
+            }
+            Acre::Trees => {
+                if count(Acre::Lumberyard) >= 3 {
+                    Acre::Lumberyard
+                } else {
+                    Acre::Trees
+                }
+            }
+            Acre::Lumberyard => {
+                if count(Acre::Lumberyard) >= 1 && count(Acre::Trees) >= 1 {
+                    Acre::Lumberyard
+                } else {
+                    Acre::OpenGround
+                }
+            }
+        }
+    }
+}
+
+struct CollectionArea {
+    inner: Vec<Vec<Acre>>,
+}
+
+impl CollectionArea {
+    fn parse(input: &[&str]) -> Result<CollectionArea, String> {
+        let inner = input
+            .iter()
+            .map(|line| line.chars().map(|c| Acre::parse(&c)).collect())
+            .collect::<Result<Vec<Vec<Acre>>, String>>()?;
+        Ok(CollectionArea { inner })
+    }
+
+    fn next(&self) -> CollectionArea {
+        let new = self
+            .inner
+            .iter()
+            .enumerate()
+            .map(|(row, line)| {
+                line.iter()
+                    .enumerate()
+                    .map(|(col, a)| {
+                        let adj = self.adjacent(row, col);
+                        a.next_state(&adj)
+                    })
+                    .collect()
+            })
+            .collect();
+        CollectionArea { inner: new }
+    }
+
+    fn adjacent(&self, row: usize, col: usize) -> Vec<&Acre> {
+        let mut res = Vec::new();
+
+        let minr = if row > 0 { row - 1 } else { row };
+        let maxr = if row < self.inner.len() - 1 {
+            row + 1
+        } else {
+            row
+        };
+        for curr_row in minr..=maxr {
+            let line = &self.inner[curr_row];
+            let minc = if col > 0 { col - 1 } else { col };
+            let maxc = if col < line.len() - 1 { col + 1 } else { col };
+            for curr_col in minc..=maxc {
+                if curr_row != row || curr_col != col {
+                    res.push(&line[curr_col]);
+                }
+            }
+        }
+        res
+    }
+
+    fn resource_value(&self) -> usize {
+        let mut trees = 0;
+        let mut lumberyards = 0;
+        for acre in self.iter() {
+            match acre {
+                Acre::OpenGround => {}
+                Acre::Trees => trees += 1,
+                Acre::Lumberyard => lumberyards += 1,
+            }
+        }
+        trees * lumberyards
+    }
+
+    fn iter(&self) -> Box<dyn Iterator<Item = Acre> + '_> {
+        Box::new(self.inner.iter().flatten().map(|a| *a))
+    }
+}
+
+fn main() {
+    let start = std::time::Instant::now();
+
+    let input = read_file("18");
+    let input: Vec<&str> = input.trim().split('\n').collect();
+
+    println!("{}", first(&input));
+    println!("{}", second(&input));
+
+    println!("elapsed {:?}", start.elapsed());
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    use lazy_static::lazy_static;
+
+    lazy_static! {
+        static ref INITIAL_STATE: Vec<&'static str> = vec![
+            ".#.#...|#.",
+            ".....#|##|",
+            ".|..|...#.",
+            "..|#.....#",
+            "#.#|||#|#|",
+            "...#.||...",
+            ".|....|...",
+            "||...#|.#|",
+            "|.||||..|.",
+            "...#.|..|.",
+        ];
+    }
+
+    #[test]
+    fn test1() {
+        let area = CollectionArea::parse(&INITIAL_STATE).unwrap();
+
+        let area = area.next();
+        let state: Vec<Acre> = area.iter().collect();
+        let expected: Vec<Acre> = ".......##.......|###.|..|...#...|#||...#..##||.|#|...#||||..||...|||..|||||.||.|||||||||||....||..|."
+            .chars().map(|c| Acre::parse(&c).unwrap()).collect();
+        assert_eq!(state, expected, "failed on step 1 with {:?}", state);
+
+        let area = area.next();
+        let state: Vec<Acre> = area.iter().collect();
+        let expected: Vec<Acre> = ".......#........|#...|.|||......##|||..#..###|||#|...#|||||.|||||||||.||||||||||||||||||||.|||||||||"
+            .chars().map(|c| Acre::parse(&c).unwrap()).collect();
+        assert_eq!(state, expected, "failed on step 2 with {:?}", state);
+
+        let area = area.next();
+        let state: Vec<Acre> = area.iter().collect();
+        let expected: Vec<Acre> = ".......#......|||#...|.||||.....###|||.#...##|||#|.||##|||||||||||||||||||||||||||||||||||||||||||||"
+            .chars().map(|c| Acre::parse(&c).unwrap()).collect();
+        assert_eq!(state, expected, "failed on step 3 with {:?}", state);
+
+        let area = area.next();
+        let state: Vec<Acre> = area.iter().collect();
+        let expected: Vec<Acre> = ".....|.#.....||||#...|.#||||....###||||#...###||#||||##|||||||||||||||||||||||||||||||||||||||||||||"
+            .chars().map(|c| Acre::parse(&c).unwrap()).collect();
+        assert_eq!(state, expected, "failed on step 4 with {:?}", state);
+
+        let area = area.next();
+        let state: Vec<Acre> = area.iter().collect();
+        let expected: Vec<Acre> = "....|||#.....||||#...|.##||||...####|||#.|.###||#||||###||||||||||||||||||||||||||||||||||||||||||||"
+            .chars().map(|c| Acre::parse(&c).unwrap()).collect();
+        assert_eq!(state, expected, "failed on step 5 with {:?}", state);
+
+        let area = area.next();
+        let state: Vec<Acre> = area.iter().collect();
+        let expected: Vec<Acre> = "...||||#.....||||#...|.###|||...#.##|||#|||#.##|#||||###||||||||#|||||||||||||||||||||||||||||||||||"
+            .chars().map(|c| Acre::parse(&c).unwrap()).collect();
+        assert_eq!(state, expected, "failed on step 6 with {:?}", state);
+
+        let area = area.next();
+        let state: Vec<Acre> = area.iter().collect();
+        let expected: Vec<Acre> = "...||||#....||#|##...|.####||.||#..##||#||##.##|#||||####||||||###||||||||||||||||||||||||||||||||||"
+            .chars().map(|c| Acre::parse(&c).unwrap()).collect();
+        assert_eq!(state, expected, "failed on step 7 with {:?}", state);
+
+        let area = area.next();
+        let state: Vec<Acre> = area.iter().collect();
+        let expected: Vec<Acre> = "..||||##....|#####..|||#####|.||#...##|#||##..###|||##.###|||||####|||||||#|||||||||||||||||||||||||"
+            .chars().map(|c| Acre::parse(&c).unwrap()).collect();
+        assert_eq!(state, expected, "failed on step 8 with {:?}", state);
+
+        let area = area.next();
+        let state: Vec<Acre> = area.iter().collect();
+        let expected: Vec<Acre> = "..||###....||#####..||##...##.||#....###|##....##|||##..###|||######|||||###||||||||||||||||||||||||"
+            .chars().map(|c| Acre::parse(&c).unwrap()).collect();
+        assert_eq!(state, expected, "failed on step 9 with {:?}", state);
+
+        let area = area.next();
+        let state: Vec<Acre> = area.iter().collect();
+        let expected: Vec<Acre> = ".||##.....||###.....||##......|##.....##|##.....##|##....##|||##.####|||#####|||||||#|||||||||||||||"
+            .chars().map(|c| Acre::parse(&c).unwrap()).collect();
+        assert_eq!(state, expected, "failed on step 10 with {:?}", state);
+
+        assert_eq!(area.resource_value(), 1147);
+    }
+}
